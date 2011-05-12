c2bf26360f22084214151fe9bee1335778db211f
Philip Papadopoulos <ppapadopoulos@ucsd.edu>
2011-05-12 00:49:41 +0000

    Rocks website theme and support images

--- src/www.rockclusters.org/version.mk
+++ src/www.rockclusters.org/version.mk
@@ -1,5 +1,11 @@
-NAME	= www.rocksclusters.org-wordpress-theme
+NAME	= rocks-website-support-files
 RELEASE	= 1
 
-PKGROOT	= /var/www/html/wordpress/wp-content/themes/rocks
+PKGROOT	= /var/www/html/
+IMAGEDIR = $(PKGROOT)/images
+WORDPRESSDIR = $(PKGROOT)/wordpress
+THEMEDIR =  $(WORDPRESSDIR)/wp-content/themes
+PLUGINDIR = $(WORDPRESSDIR)/wp-content/plugins
+
+
 
