55c6a8413d8bb999c10438cf4cdac0e5eadd854d
Philip Papadopoulos <ppapadopoulos@ucsd.edu>
2014-11-21 18:56:15 -0800

    Copyright Storm for 6.2

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,45 +5,44 @@
 # $Id: bootstrap.sh,v 1.3 2012/11/27 00:49:07 phil Exp $
 #
 # @Copyright@
-#
-# Rocks(r)
-# www.rocksclusters.org
-# version 5.6 (Emerald Boa)
-# version 6.1 (Emerald Boa)
-#
-# Copyright (c) 2000 - 2013 The Regents of the University of California.
-# All rights reserved.
-#
+# 
+# 				Rocks(r)
+# 		         www.rocksclusters.org
+# 		         version 6.2 (SideWinder)
+# 
+# Copyright (c) 2000 - 2014 The Regents of the University of California.
+# All rights reserved.	
+# 
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are
 # met:
-#
+# 
 # 1. Redistributions of source code must retain the above copyright
 # notice, this list of conditions and the following disclaimer.
-#
+# 
 # 2. Redistributions in binary form must reproduce the above copyright
 # notice unmodified and in its entirety, this list of conditions and the
-# following disclaimer in the documentation and/or other materials provided
+# following disclaimer in the documentation and/or other materials provided 
 # with the distribution.
-#
+# 
 # 3. All advertising and press materials, printed or electronic, mentioning
-# features or use of this software must display the following acknowledgement:
-#
-# "This product includes software developed by the Rocks(r)
-# Cluster Group at the San Diego Supercomputer Center at the
-# University of California, San Diego and its contributors."
-#
+# features or use of this software must display the following acknowledgement: 
+# 
+# 	"This product includes software developed by the Rocks(r)
+# 	Cluster Group at the San Diego Supercomputer Center at the
+# 	University of California, San Diego and its contributors."
+# 
 # 4. Except as permitted for the purposes of acknowledgment in paragraph 3,
 # neither the name or logo of this software nor the names of its
 # authors may be used to endorse or promote products derived from this
-# software without specific prior written permission. The name of the
+# software without specific prior written permission.  The name of the
 # software includes the following terms, and any derivatives thereof:
-# "Rocks", "Rocks Clusters", and "Avalanche Installer". For licensing of
-# the associated name, interested parties should contact Technology
-# Transfer & Intellectual Property Services, University of California,
-# San Diego, 9500 Gilman Drive, Mail Code 0910, La Jolla, CA 92093-0910,
+# "Rocks", "Rocks Clusters", and "Avalanche Installer".  For licensing of 
+# the associated name, interested parties should contact Technology 
+# Transfer & Intellectual Property Services, University of California, 
+# San Diego, 9500 Gilman Drive, Mail Code 0910, La Jolla, CA 92093-0910, 
 # Ph: (858) 534-5815, FAX: (858) 534-7345, E-MAIL:invent@ucsd.edu
-#
+# 
 # THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS''
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
 # THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
@@ -55,7 +54,7 @@
 # WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
 # OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
 # IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
+# 
 # @Copyright@
 #
 
